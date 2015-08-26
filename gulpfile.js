f1b775261db52aa182452e2f062b9b07cb061550
nide <nenegoro@gmail.com>
2015-08-26 17:36:19 +0300

    Linted gulpfil (#14)

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,54 @@
-var _ = require('underscore');
-var gulp = require('gulp');
-var historyApiFallback = require('connect-history-api-fallback');
-var browserify = require('browserify');
-var browserifyHandlebars = require('browserify-handlebars');
-var depcheck = require('depcheck');
-var watchify = require('watchify');
-var resolve = require('resolve');
-var glob = require('glob').sync;
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var rename = require('gulp-rename');
-var ghPages = require('gulp-gh-pages');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
-var path = require('path');
-var streamqueue = require('streamqueue');
+var
+  _ = require('underscore'),
+  browserify = require('browserify'),
+  browserifyHandlebars = require('browserify-handlebars'),
+  buffer = require('vinyl-buffer'),
+  concat = require('gulp-concat'),
+  depcheck = require('depcheck'),
+  ghPages = require('gulp-gh-pages'),
+  glob = require('glob').sync,
+  gulp = require('gulp'),
+  historyApiFallback = require('connect-history-api-fallback'),
+  minifyCss = require('gulp-minify-css'),
+  path = require('path'),
+  rename = require('gulp-rename'),
+  resolve = require('resolve'),
+  source = require('vinyl-source-stream'),
+  streamqueue = require('streamqueue'),
+  uglify = require('gulp-uglify'),
+  watchify = require('watchify');
 
-var baseDir = './os-upload';
-var srcDir = baseDir + '/src';
-var distDir = baseDir + '/dist';
-var stylesDir = srcDir + '/styles';
-var scriptsDir = srcDir + '/scripts';
-var frontendDependencies = _.keys(require('./package.json').dependencies);
+var
+  baseDir = './os-upload',
+  distDir = baseDir + '/dist',
+  frontendDependencies = _.keys(require('./package.json').dependencies),
 
+  /* eslint-disable sort-vars */
+  srcDir = baseDir + '/src',
 
-/**
- * Provide frontend app as a single bundle.
- */
+  scriptsDir = srcDir + '/scripts',
+  stylesDir = srcDir + '/styles',
 
-var bundler = browserify({
-  entries: [
-      scriptsDir + '/app.js',
-      // glob(scriptsDir + '/components/*.js')
-  ],
-  transform: ['browserify-handlebars'],
-  debug: true,
-  cache: {},
-  packageCache: {},
-  fullPaths: true
-});
+  // Provide frontend app as a single bundle.
+  bundler = browserify({
+    cache       : {},
+    debug       : true,
+    entries     : [scriptsDir + '/app.js'],
+    fullPaths   : true,
+    packageCache: {},
+    transform   : ['browserify-handlebars']
+  });
 
+ /*eslint-enable sort-vars */
 // Don't include vendor dependencies in this bundle
 bundler.external(frontendDependencies);
 
+// Run and return the scripts pipeline on bundle
 function scriptPipeline(bundle, outfile, options) {
-
-  /**
-   * Run and return the scripts pipeline on bundle
-   */
-
   console.log('Bundling: ' + outfile);
 
-  var outBundle = bundle.pipe(source(outfile)).pipe(buffer());
+  var
+    outBundle = bundle.pipe(source(outfile)).pipe(buffer());
 
   if(options && options.uglify)
     outBundle = outBundle.pipe(uglify());
@@ -60,47 +56,30 @@ function scriptPipeline(bundle, outfile, options) {
   return outBundle.pipe(gulp.dest(distDir));
 }
 
+// Provide frontend dependencies as a single bundle.
+gulp.task('vendor-scripts', function() {
+  var
+    bundler = browserify({});
 
-gulp.task('vendor-scripts', function () {
-
-  /**
-   * Provide frontend dependencies as a single bundle.
-   */
-
-  var bundler = browserify({});
-
-  frontendDependencies.forEach(function (id) {
-    bundler.require(resolve.sync(id), {expose: id});
-  });
-
+  frontendDependencies.forEach(function(id) { bundler.require(resolve.sync(id), {expose: id}); });
   return scriptPipeline(bundler.bundle(), 'vendor.min.js', {uglify: true});
-
 });
-
 
 gulp.task('app-scripts', function() {
   return scriptPipeline(bundler.bundle(), 'app.min.js', {uglify: true});
 });
 
-
 gulp.task('app-scripts-watched', function() {
-  var watcher  = watchify(bundler);
+  var
+    watcher = watchify(bundler);
 
-
-  watcher
-    .on('update', function() {
-      scriptPipeline(watcher.bundle(), 'app.min.js');
-    });
-
+  watcher .on('update', function() { scriptPipeline(watcher.bundle(), 'app.min.js'); });
   return scriptPipeline(watcher.bundle(), 'app.min.js');
 });
 
-
-gulp.task('check-deps', function () {
-  depcheck(path.resolve('./'), {
-    'withoutDev': false,
-    'ignoreDirs': ['dist', 'node_modules']
-  }, function(U) {
+// List unused dependencies in stderr.
+gulp.task('check-deps', function() {
+  depcheck(path.resolve('./'), {withoutDev: false, ignoreDirs: ['dist', 'node_modules']}, function(U) {
     if(!_.isEmpty(U.dependencies))
       console.error('Unused dependencies: ', U.dependencies.join(', '));
 
@@ -112,20 +91,13 @@ gulp.task('check-deps', function () {
   });
 });
 
-
-gulp.task('styles', function () {
-  /**
-   * Provide frontend styles as a single bundle.
-   */
-  return streamqueue(
-    {objectMode: true},
-    gulp.src(stylesDir + '/app.css')
-  )
+// Provide frontend styles as a single bundle.
+gulp.task('styles', function() {
+  return streamqueue({objectMode: true}, gulp.src(stylesDir + '/app.css'))
     .pipe(concat('app.min.css'))
     .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(gulp.dest(distDir));
 });
-
 
 gulp.task('default', ['vendor-scripts', 'app-scripts', 'styles']);
 gulp.task('dev', ['vendor-scripts', 'app-scripts-watched', 'styles']);
