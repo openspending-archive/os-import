af5328e5a214ca026613821f0daa148b1bc46bf9
nide <nenegoro@gmail.com>
2015-08-31 14:50:08 +0300

    Rendering step 1 header (#11)

--- os-import/src/scripts/app.js
+++ os-import/src/scripts/app.js
@@ -1,9 +1,12 @@
+// Required during objects init below
+window.TEMPLATES = require('../templates');
+
 var
   backbone = require('backbone'),
   Router = require('./router'),
   SheIsAliveView = require('./components/ui/sheisalive');
 
-window.APP = new SheIsAliveView({el: '#application'});
+window.APP = new SheIsAliveView({el: 'body'});
 window.ROUTER = new Router();
 window.APP.render();
 backbone.history.start({pushState: true});
---
+++ os-import/src/templates/index.js
@@ -0,0 +1,6 @@
+var
+  _ = require('lodash');
+
+module.exports = {
+  'create-dp/header.hbs': require('./create-dp/header.hbs')
+};
