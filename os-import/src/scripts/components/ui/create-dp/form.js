232b90d7ca86804858a3701584fb0623fe57b49f
nide <nenegoro@gmail.com>
2015-08-31 18:46:18 +0300

    Upload file, parse it and throw the event when all is done (#11)

--- os-import/src/scripts/components/ui/create-dp/form.js
+++ os-import/src/scripts/components/ui/create-dp/form.js
@@ -1,4 +1,5 @@
 var
+  _ = require('lodash'),
   backbone = require('backbone'),
 
   /* eslint-disable no-unused-vars */
@@ -8,6 +9,34 @@ var
   UploadView = require('./upload');
 
 module.exports = backbone.BaseView.extend({
+  activate: function(state) {
+    backbone.BaseView.prototype.activate.call(this, state);
+
+    if((_.isUndefined(state) || state) && _.isEmpty(this.layout))
+      this.render();
+
+    // Do not allow chuld view (upload widget) to change parent — just catch its events
+    this.layout.upload.on('upload-started', this.loading, this);
+
+    this.layout.upload.on('parse-complete', function(csvData) {
+      // TODO Apply data to the form
+      console.log(csvData);
+      this.loading(false);
+    }, this);
+
+    return this;
+  },
+
+  loading: function(state) {
+    var
+      isLoading = _.isUndefined(state) || state;
+
+    this.$('[data-id=submit]').toggleClass('form-button--loading', isLoading);
+    this.$('[data-id=upload-in-progress]').prop('hidden', !isLoading);
+    this.$('[data-id=create]').prop('hidden', isLoading);
+    return this;
+  },
+
   render: function() {
     this.$el.html(this.template({}));
     this.layout.upload = (new UploadView({el: this.$('[data-id=upload]'), parent: this})).render();
--- os-import/src/scripts/components/ui/create-dp/upload.js
+++ os-import/src/scripts/components/ui/create-dp/upload.js
@@ -1,15 +1,81 @@
+require('fileapi');
+
 var
+  _ = require('lodash'),
   backbone = require('backbone'),
 
   /* eslint-disable no-unused-vars */
-  backboneBase = require('backbone-base');
+  backboneBase = require('backbone-base'),
 
-/* eslint-enable no-unused-vars */
+  /* eslint-enable no-unused-vars */
+  csv = require('csv'),
+  jtsInfer = require('json-table-schema').infer,
+  Promise = require('bluebird'),
+  request = require('superagent-bluebird-promise'),
+  validator = require('validator');
+
 module.exports = backbone.BaseView.extend({
-  render: function() {
-    this.$el.html(this.template({}));
+  events: {
+    'change [data-id=file]': function(event) {
+       this.trigger('upload-started');
+
+      FileAPI.readAsText(FileAPI.getFiles(event.currentTarget)[0], (function(fileEvent) {
+        if(fileEvent.type === 'load') {
+          this.parseCSV(fileEvent.target.name, fileEvent.result);
+        } else if(fileEvent.type !== 'progress') {
+          this.trigger('upload-error');
+
+          // TODO Implement upload errors rendering
+          this.setError('File upload failed');
+        }
+
+        this.$(event.currentTarget).val('');
+      }).bind(this));
+    },
+
+    'click [data-id=select-file]': function() { this.$('[data-id=file]').trigger('click'); },
+
+    'keydown [data-id=link]': function(event) {
+      var
+        url = this.$('[data-id=link]').val();
+
+      if(event.keyCode !== 13)
+        return true;
+
+      if(!validator.isURL(url)) {
+        console.error('URL has worng format: ' + url);
+        return false;
+      }
+
+      // TODO Filter non-csv data with appropriate error message
+      // Download data file and trigger event to let parent form to catch it up
+      request.get(url)
+        .then((function(result) { this.parseCSV(_.last(url.split('/')), result.text); }).bind(this))
+        .catch(function(error) { console.error('Error while downloading file from url: ' + error); });
+
+      return false;
+    }
+  },
+
+  parseCSV: function(name, string) {
+    csv.parse(string, (function(error, data) {
+      // TODO Implement parse errors rendering
+      if(error) {
+        console.log(error);
+        this.trigger('parse-error');
+        return false;
+      }
+
+      this.trigger('parse-complete', {
+        data: data,
+        name: name,
+        schema: jtsInfer(data[0], _.rest(data))
+      });
+    }).bind(this));
+
     return this;
   },
 
+  render: function() { this.$el.html(this.template({})); return this; },
   template: window.TEMPLATES['create-dp/upload.hbs']
 });
