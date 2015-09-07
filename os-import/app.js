d46c31d3456b8e0b8486d4289f3fc1856c2175e6
nide <nenegoro@gmail.com>
2015-09-07 16:00:21 +0300

    Formatted app.js

--- os-import/app.js
+++ os-import/app.js
@@ -1,25 +1,23 @@
-var
-  express = require('express'),
-  path = require('path'),
-  SSI = require('node-ssi');
-
-var
-  app = express(),
-
-  /* eslint-disable sort-vars */
-  templatesDir = path.join(__dirname, 'src', 'templates'),
-
-  /* eslint-enable sort-vars */
-  ssi = new SSI({baseDir: templatesDir});
-
+var express = require('express');
+var app = express();
+var path = require('path');
+var SSI = require('node-ssi');
+var templatesDir = path.join(__dirname, 'src', 'templates');
+var ssi = new SSI({baseDir: templatesDir});
 app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('/', function(request, response) {
-	ssi.compileFile(path.join(templatesDir, 'index.html'), function(err, content) { response.send(content); });
+  ssi.compileFile(
+    path.join(templatesDir, 'index.html'),
+    function(err, content) { response.send(content); }
+  );
 });
 
 app.get('*', function(request, response) {
-	ssi.compileFile(path.join(templatesDir, 'app.html'), function(err, content) { response.send(content); });
+  ssi.compileFile(
+    path.join(templatesDir, 'app.html'),
+    function(err, content) { response.send(content); }
+  );
 });
 
 module.exports = app;
