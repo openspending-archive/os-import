7238987767f615fc7871286f0428ffad7674c33a
nide <nenegoro@gmail.com>
2015-09-07 14:59:12 +0300

    /templates/index.js formatted

--- os-import/src/templates/index.js
+++ os-import/src/templates/index.js
@@ -1,11 +1,22 @@
 module.exports = {
-  'create-dp/data-file-editor.hbs'      : require('./create-dp/data-file-editor.hbs'),
-  'create-dp/data-file-editor-item.hbs' : require('./create-dp/data-file-editor-item.hbs'),
-  'datapackage.hbs'                     : require('./datapackage.hbs'),
-  'create-dp/form.hbs'                  : require('./create-dp/form.hbs'),
-  'create-dp/header.hbs'                : require('./create-dp/header.hbs'),
-  'create-dp/name-editor.hbs'           : require('./create-dp/name-editor.hbs'),
-  'create-dp/validation-report.hbs'     : require('./create-dp/validation-report.hbs'),
-  'create-dp/validation-report-item.hbs': require('./create-dp/validation-report-item.hbs'),
-  'create-dp/upload.hbs'                : require('./create-dp/upload.hbs')
+  'create-dp/data-file-editor.hbs': require('./create-dp/data-file-editor.hbs'),
+
+  'create-dp/data-file-editor-item.hbs': require(
+    './create-dp/data-file-editor-item.hbs'
+  ),
+
+  'datapackage.hbs'          : require('./datapackage.hbs'),
+  'create-dp/form.hbs'       : require('./create-dp/form.hbs'),
+  'create-dp/header.hbs'     : require('./create-dp/header.hbs'),
+  'create-dp/name-editor.hbs': require('./create-dp/name-editor.hbs'),
+
+  'create-dp/validation-report.hbs': require(
+    './create-dp/validation-report.hbs'
+  ),
+
+  'create-dp/validation-report-item.hbs': require(
+    './create-dp/validation-report-item.hbs'
+  ),
+
+  'create-dp/upload.hbs': require('./create-dp/upload.hbs')
 };
