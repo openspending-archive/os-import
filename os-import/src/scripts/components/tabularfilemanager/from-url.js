1666d43ad6e83b3fa390975d6d149545b5042030
nide <nenegoro@gmail.com>
2015-09-23 18:13:09 +0300

    Tabular file manager with URL upload implemented (#30)

--- os-import/src/scripts/components/tabularfilemanager/from-url.js
+++ os-import/src/scripts/components/tabularfilemanager/from-url.js
@@ -1,1 +1,29 @@
-module.exports = function(url) { return this; }
+module.exports = function(url, doneCb, errorCb) {
+  if(!require('validator').isURL(url)) {
+    errorCb(null, 'URL has worng format: ' + url);
+    return false;
+  }
+
+  this.emit('upload-started');
+
+  // TODO Filter non-csv data with appropriate error message
+  // Download data file and trigger event to let parent form to catch it up
+  require('superagent-bluebird-promise').get(url)
+    .then((function(result) {
+      this.parse({
+        content: result.text,
+        name: url,
+        size: result.header['content-length'] || 0
+      }, {
+        doneCb: doneCb,
+        errorCb: errorCb,
+        isURL: true
+      });
+    }).bind(this))
+
+    .catch(function(error) {
+      errorCb(null, 'Error while downloading file from url: ' + error);
+    });
+
+  return this; 
+}
--- os-import/src/scripts/components/ui/step1/upload.js
+++ os-import/src/scripts/components/ui/step1/upload.js
@@ -1,16 +1,23 @@
 require('backbone-base');
 var _ = require('lodash');
 var backbone = require('backbone');
-var request = require('superagent-bluebird-promise');
 var TabularFileManager = require('../../tabularfilemanager');
-var validator = require('validator');
 
 module.exports = backbone.BaseView.extend({
+  // Append data to the data files list of the Step 1 form
+  addFile: function(data) {
+    var form = this.parent.layout.form;
+    form.setValue('files',(form.getValue().files || []).concat(data));
+    return this;
+  },
+
   events: {
     'change [data-id=file]': function(event) {
-      this.fileManager.fromBlob(event.currentTarget.files[0], (function(data) {
-        this.setValue('files', (this.getValue().files || []).concat(data));
-      }).bind(this.parent.layout.form));
+      // When file uploaded add it to the Step 1 form
+      this.fileManager.fromBlob(
+        event.currentTarget.files[0],
+        this.addFile.bind(this)
+      );
 
       this.$(event.currentTarget).val('');
     },
@@ -26,29 +33,7 @@ module.exports = backbone.BaseView.extend({
         return true;
 
       this.$('[data-id=link]').val('');
-
-      if(!validator.isURL(url)) {
-        console.error('URL has worng format: ' + url);
-        return false;
-      }
-
-      this.trigger('upload-started');
-
-      // TODO Filter non-csv data with appropriate error message
-      // Download data file and trigger event to let parent form to catch it up
-      request.get(url)
-        .then((function(result) {
-          this.parseCSV({
-            content: result.text,
-            name: url,
-            size: result.header['content-length'] || 0
-          }, {isURL: true});
-        }).bind(this))
-
-        .catch(function(error) {
-          console.error('Error while downloading file from url: ' + error);
-        });
-
+      this.fileManager.fromURL(url, this.addFile.bind(this));
       return false;
     }
   },
